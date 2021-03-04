47ae42af78b1ff047be1f3944ef004c9f565774c
Simranjit Singh Gulati <simr0026@zbc.dk>
2021-03-04 09:52:48 +0100

    Add project files.

---
+++ Jedi mind tricks OPG/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jedi_mind_tricks_OPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> person = new Dictionary<string, int>();
+
+            person.Add("Alex", 19);
+
+            person["navn"] = 78;
+
+            Console.WriteLine(person.First());
+            
+            Dictionary<string, bool> persons = new Dictionary<string, bool>()
+            {
+                { "Luke", true },
+                { "Han", false },
+                { "Chewbacca", false }
+            };
+
+            persons.Remove("Han");
+
+            foreach (KeyValuePair<string, bool> item in persons)
+            {
+                Console.WriteLine($"{item.Key}, {item.Value}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
